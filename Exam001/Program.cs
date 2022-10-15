dda7b56c5c9095ab567b262e9a5d7c3c392972a8
gusev <pjack86@mail/com>
2022-10-15 18:51:49 +0500

    new

---
+++ Exam001/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+class Program
+{
+  static void Foo (int i)
+  {
+    Console.WriteLine(i);
+    if (i <= 1)
+    return;
+    
+    i--;
+    Foo(i);
+  }
+
+  static void Main(string[] args)
+  {
+    Foo(8);
+  }
+}
---
+++ Exam002/Program.cs
@@ -0,0 +1,10 @@
+﻿int m = 1;
+int n = 15;
+int a = 0;
+
+while (m<=n)
+{
+    a = a + m;
+    m++;
+}
+Console.WriteLine(a);
